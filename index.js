231009c7e41420853ddbb878a634dc526bd3209f
AndresManjarres <89228795+AndresManjarres@users.noreply.github.com>
2023-11-03 10:23:19 -0500

    Correccion de dealy y fecha

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 // Andres manjarres - 2190826
 
 const {Browser, Builder, By} = require("selenium-webdriver");
-
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const start = async () => {
   let driver = null;
-  try{
-    driver = await new Builder().forBrowser(Browser.CHROME).build();
 
+  try{
+
+    // Tipo de navegador que se va a utilizar (Varable)
+    driver = await new Builder().forBrowser(Browser.CHROME).build()
+    // URL de la pagina de pruebas de selenium (Variable)
     await driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
     //1
     const textarea = await driver.findElement(By.css('textarea.form-control[name="my-textarea"]'));
-    await textarea.sendKeys('anita lava la tina.');
     await delay(2000);
-
+    await textarea.sendKeys('anita lava la tina.');
+    
     //2
     const selectElement = await driver.findElement(By.css('select.form-select[name="my-select"]'));
     const optionThree = await selectElement.findElement(By.css('option[value="3"]'));
@@ -31,7 +33,8 @@ const start = async () => {
     //4
     const datePicker = await driver.findElement(By.css('input[type="text"][name="my-date"]'));
     await delay(2000);
-    await datePicker.sendKeys('16/08/1970');
+    // Formato de fecha: MM/DD/AAAA
+    await datePicker.sendKeys('8/16/1970');
 
     //5
     const checkbox = await driver.findElement(By.id('my-check-2'));
@@ -40,18 +43,18 @@ const start = async () => {
 
     //6
     const submit = await driver.findElement(By.css('button[type="submit"]'));
-    await delay(5000);
+    await delay(2000);
     await submit.click();
 
     //7
     const titleElement = await driver.findElement(By.css('h1.display-6'));
     const titleText = await titleElement.getText();
-    console.log(`Aqui sale ${titleText}`);
+    console.log(`------ Aqui sale (${titleText})`);
 
     //8
     const textResult = await driver.findElement(By.id('message'));
     const textValue = await textResult.getText();
-    console.log(`Aqui sale ${textValue}`);
+    console.log(`------ Aqui sale (${textValue})`);
 
     console.log(submit);
 
